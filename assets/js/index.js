f1578d6f5325f2578c6556858a30e073d84f8732
murraydeam <73445952+murraydeam@users.noreply.github.com>
2022-11-09 12:05:40 -0500

    all changes updated. the styling is simple & will need to be refined

---
+++ assets/js/index.js
@@ -0,0 +1,14 @@
+var password = document.getElementById('pass'), confirm_password = 
+document.getElementById('confirm-pass');
+
+function validatePassword(){
+    if(password.value != confirm_password.value) {
+        confirm_password.setCustomValidity('Passwords do not match');
+    } else {
+        confirm_password.setCustomValidity('');
+    }
+}
+
+password.onchange = validatePassword;
+confirm_password.onkeyup = 
+validatePassword;
